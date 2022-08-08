fd0756fc1794c68c57b2108bd71a71ce64209d5b
築城大知 <lmi2022-07@la-study.com>
2022-08-08 15:40:09 +0900

    ログイン機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+  before_action :require_logged_in, only: [:show, :edit, :update]
+  before_action :already_logged_in, only: [:new, :create]
   def show
-    @user = User.last
+    @user = current_user
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to:'sessions#destroy'
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/destroy'
   root to: 'users#show'
   get 'users/show'
   get 'signup', to: 'users#new'
